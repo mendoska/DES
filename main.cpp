1acf267753062b874e906befe2fb50087523cbee
Alex Mendoza <alexandermen2017@fau.edu>
2023-03-01 21:28:32 -0500

    created xor function, started working on keys function, right expansion.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,51 @@
 
 std::string DES();
 
+std::string xorString(std::string x, std::string y){
+    std::string result ="";
+    int size = y.size();
+    //essentially running through a loop of size y that checks 0,1 in string form
+    for (int i =0; i<size; i++){
+        if (x[i] != y[i]){
+            result += "1";
+        }
+        else {
+            result+="0";
+        }
+    }
+    return result;
+}
+
+void keys(std::string key){
+    //PC1 Table
+    int pc1[56]{
+        57,49,41,33,25,17,9,
+        1,58,50,42,34,26,18,
+        10,2,59,51,43,35,27,
+        19,11,3,60,52,44,36,
+        63,55,47,39,31,23,15,
+        7,62,54,46,38,30,22,
+        14,6,61,53,45,37,29,
+        21,13,5,28,20,12,4
+    };
+
+    //PC2 Table
+    int pc2[48] = {
+        14,17,11,24,1,5,
+        3,28,15,6,21,10,
+        23,19,12,4,26,8,
+        16,7,27,20,13,2,
+        41,52,31,37,47,55,
+        30,40,51,45,33,48,
+        44,49,39,56,34,53,
+        46,42,50,36,29,32
+    };
+    
+    
+}
+
+
+
 
 std::string DES(std::string plainTxt){
     
@@ -116,6 +161,13 @@ std::string DES(std::string plainTxt){
     std::string left = initPerm.substr(0,32);
     std::string right = initPerm.substr(32,32);
 
+    //encrypt 16 times
+    for (int i=0; i<16; i++){
+        //need to expand right side from 32 bits to 48 bits
+        std::string rightExpansion = "";
+        rightExpansion += right[EXPANSION[i]-1];
+    }
+    
     return 0;
 }
 
